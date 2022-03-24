9778e976f16b1ed4a75a7a667e93460dd51db4b9
marlon023562 <102229130+marlon023562@users.noreply.github.com>
2022-03-24 05:26:51 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Marlon </title>
+	<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">			
+	<link href="css/mystyle.css" rel="stylesheet">			
+   </head>
+
+
+        
+            
+ <!-- page content -->
+        <div class="right_col" role="main">
+  <div id="main"></div>
+
+	 <script src="../bootstrap/js/jquery-1.11.3.js"></script>
+	<script src="../bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/myscript.js"></script>
+  </body>
+ 
+</html>
